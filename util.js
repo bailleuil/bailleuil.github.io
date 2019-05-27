f3211a9adc041e348cf09ea942e7da9f1e3096b4
coralie bailleuil <coralie.bailleuil@coding-academy.fr>
2019-05-27 14:03:36 +0200

    form

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 const { PI, cos, sin, abs, sqrt, pow, round, random, atan2 } = Math;
-const HALF_PI = 0.5 * PI;
+const HALF_PI = 0.5 * PI;/*const HALF_PI = 0.5 * PI;*/
 const TAU = 2 * PI;
 const TO_RAD = PI / 180;
 const floor = n => n | 0;
@@ -13,5 +13,5 @@ const fadeInOut = (t, m) => {
 	return abs((t + hm) % m - hm) / (hm);
 };
 const dist = (x1, y1, x2, y2) => sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-const angle = (x1, y1, x2, y2) => atan2(y2 - y1, x2 - x1);
+const angle = (x1, y1, x2, y2) => atan2(y2 - y1, x2 - x1);/*const angle = (x1, y1, x2, y2) => atan2(y2 - y1, x2 - x1);*/
 const lerp = (n1, n2, speed) => (1 - speed) * n1 + speed * n2;
